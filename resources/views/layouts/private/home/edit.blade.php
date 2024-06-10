a6ee0698b9ae9110ce0775e5708b8605cfd074f8
Gresati <gresatiati6@gmail.com>
2024-06-10 20:49:06 +0700

    Update edit.blade.php

--- resources/views/layouts/private/home/edit.blade.php
+++ resources/views/layouts/private/home/edit.blade.php
@@ -8,13 +8,16 @@
                 <div class="card-header">Edit Data Home</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('home.update', $home->id) }}">
+                    <form method="POST" action="{{ route('home.update', $home->id) }}" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
 
                         <div class="form-group">
                             <label for="hero">Hero</label>
-                            <input id="hero" type="text" class="form-control @error('hero') is-invalid @enderror" name="hero" value="{{ $home->hero }}" required autofocus>
+                            <div>
+                                <img src="{{ Storage::url($home->hero) }}" alt="Hero" style="width: 100px;">
+                            </div>
+                            <input id="hero" type="file" class="form-control @error('hero') is-invalid @enderror" name="hero">
                             @error('hero')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -34,7 +37,10 @@
 
                         <div class="form-group">
                             <label for="foto1">Foto 1</label>
-                            <input id="foto1" type="text" class="form-control @error('foto1') is-invalid @enderror" name="foto1" value="{{ $home->foto1 }}" required>
+                            <div>
+                                <img src="{{ Storage::url($home->foto1) }}" alt="Foto 1" style="width: 100px;">
+                            </div>
+                            <input id="foto1" type="file" class="form-control @error('foto1') is-invalid @enderror" name="foto1">
                             @error('foto1')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -44,7 +50,10 @@
 
                         <div class="form-group">
                             <label for="foto2">Foto 2</label>
-                            <input id="foto2" type="text" class="form-control @error('foto2') is-invalid @enderror" name="foto2" value="{{ $home->foto2 }}" required>
+                            <div>
+                                <img src="{{ Storage::url($home->foto2) }}" alt="Foto 2" style="width: 100px;">
+                            </div>
+                            <input id="foto2" type="file" class="form-control @error('foto2') is-invalid @enderror" name="foto2">
                             @error('foto2')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -54,7 +63,10 @@
 
                         <div class="form-group">
                             <label for="foto3">Foto 3</label>
-                            <input id="foto3" type="text" class="form-control @error('foto3') is-invalid @enderror" name="foto3" value="{{ $home->foto3 }}" required>
+                            <div>
+                                <img src="{{ Storage::url($home->foto3) }}" alt="Foto 3" style="width: 100px;">
+                            </div>
+                            <input id="foto3" type="file" class="form-control @error('foto3') is-invalid @enderror" name="foto3">
                             @error('foto3')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
